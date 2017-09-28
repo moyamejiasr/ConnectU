3f129ef028ab012e249cb2994b5696ab4a000510
Onelio <rmoya98@gmail.com>
2017-09-29 00:58:29 +0200

    Bugfix pre-release

--- app/src/main/java/com/onelio/connectu/Activities/Apps/Materiales/MaterialesDialog.java
+++ app/src/main/java/com/onelio/connectu/Activities/Apps/Materiales/MaterialesDialog.java
@@ -8,6 +8,7 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
 import android.support.v7.widget.CardView;
 import android.view.View;
 import android.view.Window;
@@ -48,6 +49,7 @@ public class MaterialesDialog extends Dialog {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
         getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.view_dialog_materiales);

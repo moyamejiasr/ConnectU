7aaa865b332b4a4756d35b8ef3ba0414bb2b9579
Onelio <rmoya98@gmail.com>
2018-09-20 14:30:23 +0200

    Added pending files

---
+++ app/src/main/java/com/onelio/connectu/activities/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.onelio.connectu.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.onelio.connectu.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

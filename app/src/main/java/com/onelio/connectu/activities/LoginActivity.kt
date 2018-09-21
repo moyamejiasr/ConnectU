33df7f73f73f7a48f00da538d5ecae5f98b632d5
Onelio <rmoya98@gmail.com>
2018-09-21 08:49:21 +0200

    Added login Activity

---
+++ app/src/main/java/com/onelio/connectu/activities/LoginActivity.kt
@@ -0,0 +1,18 @@
+package com.onelio.connectu.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.onelio.connectu.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun onLoginClick(view : View) {
+
+    }
+}
